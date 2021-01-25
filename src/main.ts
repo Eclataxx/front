7dd5248a6c789e673c0197bc1a2c71008e84581d
Bbeauprez <basile.beauprez.59@gmail.com>
2021-01-25 21:09:21 +0100

    added toast lib and code example

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,23 @@
 import { createApp } from 'vue';
+import Toast, { PluginOptions, useToast } from 'vue-toastification';
+import 'vue-toastification/dist/index.css';
 import App from './App.vue';
+// eslint-disable-next-line import/extensions,import/no-unresolved
 import router from './router';
+// eslint-disable-next-line import/extensions,import/no-unresolved
 import store from './store';
 import './assets/css/tailwind.css';
 
-createApp(App).use(store).use(router).mount('#app');
+const options: PluginOptions = {
+
+};
+
+createApp(App).use(store).use(router).use(Toast, options)
+  .mount('#app');
+
+/*
+  how to call toast
+*to remove when merging on main
+  const toast = useToast();
+  toast("I'm a toast!");
+*/
