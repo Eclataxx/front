6dfbd2470ebe9bdbf8cdc0bd019ad2bb64e58ce1
Tom Délié <tomdelie78@gmail.com>
2021-01-26 21:59:00 +0100

    Remove purge css

--- postcss.config.js
+++ postcss.config.js
@@ -4,16 +4,15 @@ const tailwindcss = require('tailwindcss');
 
 const purgecss = require('@fullhuman/postcss-purgecss')({
   content: [
-    './index.html',
+    './public/index.html',
     './src/**/*.{vue,js,ts,jsx,tsx}',
   ],
-  defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || []
+  defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
 });
 
 module.exports = {
   plugins: [
     tailwindcss,
     autoprefixer,
-    purgecss,
   ],
 };
