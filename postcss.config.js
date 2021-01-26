c46f411939220730fbb4f6a08da90dc3b1b56093
Tom Délié <tomdelie78@gmail.com>
2021-01-26 21:20:24 +0100

    Add purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -2,9 +2,18 @@
 const autoprefixer = require('autoprefixer');
 const tailwindcss = require('tailwindcss');
 
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: [
+    './index.html',
+    './src/**/*.{vue,js,ts,jsx,tsx}',
+  ],
+  defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || []
+});
+
 module.exports = {
   plugins: [
     tailwindcss,
     autoprefixer,
+    purgecss,
   ],
 };
